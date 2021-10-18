9ec1cbfe4bc7cfa6acba03518170fc29667127f2
saiprasaths <saiprasathpgm2003@gmail.com>
2021-10-18 19:49:09 +0530

    add restaurant,food,menu route

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import passport from "passport";
-
 // configs
 import googleAuthConfig from "./config/google.config";
 
 // microservice routes
 import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+import Food from "./API/Food";
+
 // Database connection
 import ConnectDB from "./database/connection";
 const zomato = express();
@@ -21,12 +23,14 @@ zomato.use(helmet());
 zomato.use(cors());
 zomato.use(passport.initialize());
 zomato.use(passport.session());
-
 // passport cofiguration
 googleAuthConfig(passport);
 
 // Application Routes
 zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/food", Food);
+
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 zomato.listen(4000, () =>
   ConnectDB()
